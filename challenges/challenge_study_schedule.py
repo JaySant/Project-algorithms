8f712869750e17ff72575b325d85e0315844f015
JaySant <jaysant15@gmail.com>
2023-04-17 19:09:12 -0300

    feat: implementando requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
-def study_schedule(permanence_period, target_time):
-    
-    raise NotImplementedError
+def study_schedule(permanence_periods, target_time):
+
+    max_students = 0
+
+    for start_time, end_time in permanence_periods:
+        try:
+            if start_time <= target_time <= end_time:
+                max_students += 1
+        except (ValueError, TypeError):
+            return None
+    return max_students
