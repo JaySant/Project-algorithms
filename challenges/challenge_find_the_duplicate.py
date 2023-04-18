7150333099ac3837f6a159067a90521c713b15bc
JaySant <jaysant15@gmail.com>
2023-04-18 02:49:25 -0300

    feat: requisito 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,11 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    verify_nums = all(isinstance(num, int)for num in nums)
+    if not nums or not verify_nums or len(nums) < 2 or min(nums) < 1:
+        return False
+
+    nums.sort()
+    for i in range(1, len(nums)):
+        if nums[i] == nums[i-1]:
+            return nums[i]
+
+    return False
