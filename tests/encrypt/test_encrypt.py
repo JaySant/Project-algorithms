a9e1c14db59982e33c66810a3f8c7e0b9590d38a
JaySant <jaysant15@gmail.com>
2023-04-17 21:39:06 -0300

    test: implementando requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(253, 0)
+    with pytest.raises(TypeError):
+        encrypt_message('message', 'key')
+
+    assert encrypt_message("python", -6) == "nohtyp"
+    assert encrypt_message("python", 4) == "no_htyp"
